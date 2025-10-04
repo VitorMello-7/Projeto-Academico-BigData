cab1ffca7e799c4e193bf0ba87cc87a5b490891e
Vitor Mello <vitormellocunha@gmail.com>
2025-10-04 15:55:01 -0300

    Adicionando Models, Migrate, .html

--- bigData/models.py
+++ bigData/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class DataRecord(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=20)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome}: {self.cnpj} at {self.timestamp}"
